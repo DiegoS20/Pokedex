008480f0ec56e19dd0e7c1e41325af9d306ae1be
Diego José Saravia Zaldaña <saravia__2000@hotmail.com>
2020-02-07 13:15:22 -0500

    Adding 'build' carpet to deploy the proyect in production

---
+++ build/precache-manifest.c6e758706bff973d29e4bb89f61d2d0d.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a7fa8125fca7007bfa627ae08ec856ce",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2e7c6d69b488c1a4344f",
+    "url": "/static/css/2.67123b20.chunk.css"
+  },
+  {
+    "revision": "0badcf52c5d0dd6db7c1",
+    "url": "/static/css/main.9b6e9acf.chunk.css"
+  },
+  {
+    "revision": "2e7c6d69b488c1a4344f",
+    "url": "/static/js/2.e1d90bb1.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/static/js/2.e1d90bb1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0badcf52c5d0dd6db7c1",
+    "url": "/static/js/main.b90f8e21.chunk.js"
+  },
+  {
+    "revision": "a3ae24c73f7b90477b95",
+    "url": "/static/js/runtime-main.801dd740.js"
+  },
+  {
+    "revision": "468f7826c935091acfb33c303733a0e5",
+    "url": "/static/media/pokeball.468f7826.png"
+  },
+  {
+    "revision": "d9031165090c1639c3769d3e0d9468d2",
+    "url": "/static/media/pokedex_text.d9031165.png"
+  }
+]);
